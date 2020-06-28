4f957f2f830aa2c7135d4e00a92b2485d02c6e02
Owen Salter <owen@devosmium.xyz>
2020-06-28 14:47:15 -0400

    feat: Add ServiceError type

---
+++ src/errors.rs
@@ -0,0 +1,28 @@
+use actix_web::{error::ResponseError, HttpResponse};
+use derive_more::Display;
+use std::convert::From;
+
+#[derive(Debug, Display)]
+pub enum ServiceError {
+    #[display(fmt = "Internal Server Error")]
+    InternalServerError,
+
+    #[display(fmt = "Bad Request: {}", _0)]
+    BadRequest(String),
+
+    #[display(fmt = "Unauthorized")]
+    Unauthorized,
+}
+
+#[cfg(feature = "actix")]
+impl ResponseError for ServiceError {
+    fn error_response(&self) -> HttpResponse {
+        match self {
+            ServiceError::InternalServerError => {
+                HttpResponse::InternalServerError().body("Internal Server Error. Try again later.")
+            }
+            ServiceError::BadRequest(s) => HttpResponse::BadRequest().body(s),
+            ServiceError::Unauthorized => HttpResponse::Unauthorized().body("Unauthorized"),
+        }
+    }
+}
