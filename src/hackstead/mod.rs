d8ecd9dd4b18dc81c5e1bc5ca1d30a6defb3deeb
Owen Salter <owen@devosmium.xyz>
2020-08-16 10:51:48 -0400

    add item_mut function to hackstead

--- src/hackstead/mod.rs
+++ src/hackstead/mod.rs
@@ -86,6 +86,16 @@ impl Hackstead {
             .ok_or_else(|| NoSuchItem(self.steader_id(), item_id))?)
     }
 
+    pub fn item_mut(&mut self, i: impl IdentifiesItem) -> NoSuchResult<&mut Item> {
+      let item_id = i.item_id();
+      let steader_id = self.steader_id();
+      Ok(self
+          .inventory
+          .iter_mut()
+          .find(|i| i.item_id == item_id)
+          .ok_or_else(|| NoSuchItem(steader_id, item_id))?)
+    }
+
     pub fn take_item(&mut self, i: impl IdentifiesItem) -> NoSuchResult<Item> {
         let (steader_id, item_id) = (self.steader_id(), i.item_id());
         let p = self
